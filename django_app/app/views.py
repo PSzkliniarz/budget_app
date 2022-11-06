91a51af0ec99356201f899703f9ea76b9516ede8
Pawel <pszkliniarz@gmail.com>
2022-11-06 16:40:21 +0100

    Added first serializer, view and urls

--- django_app/app/views.py
+++ django_app/app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics
+from .models import Account
+from .serializers import AccountSerializer
 
-# Create your views here.
+
+class AccountsView(generics.RetrieveAPIView):
+    queryset = Account.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = AccountSerializer(queryset, many=True)
+        return Response(serializer.data)
