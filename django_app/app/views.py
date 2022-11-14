5cd80162fdc2a167a78e423c805b038a8caad9ac
Pawel <pszkliniarz@gmail.com>
2022-11-14 16:35:23 +0100

    Added simplejwt authentication to backend

--- django_app/app/views.py
+++ django_app/app/views.py
@@ -2,9 +2,11 @@ from rest_framework.response import Response
 from rest_framework import generics
 from .models import Account
 from .serializers import AccountSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class AccountsView(generics.RetrieveAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Account.objects.all()
 
     def get(self, request, *args, **kwargs):
