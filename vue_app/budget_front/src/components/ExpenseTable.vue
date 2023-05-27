5bc2504d3730578bbcf9d124767da11501f6f2ce
Pawel <pszkliniarz@gmail.com>
2023-05-27 18:25:14 +0200

    Add proper data to table

--- vue_app/budget_front/src/components/ExpenseTable.vue
+++ vue_app/budget_front/src/components/ExpenseTable.vue
@@ -1,150 +1,56 @@
 <template>
   <div>
-    <h1>Table</h1>
+    <h1>Wydatki</h1>
     <v-data-table
-    :headers="headers"
-    :items="desserts"
-    :items-per-page="5"
-    class="elevation-1"
-    :search="search"
-  >
+        :headers="headers"
+        :items="getExpenses || []"
+        :items-per-page="5"
+        class="elevation-1"
+        :search="search"
+    >
       <template v-slot:top>
         <v-text-field
-          v-model="search"
-          label="Search (UPPER CASE ONLY)"
-          class="mx-4"
+            v-model="search"
+            label="Szukaj wydatku"
+            class="mx-4"
         ></v-text-field>
       </template>
-      <template v-slot:[`item.name`]="{ item }">
-        {{ item.name }}
+      <template v-slot:[`item.title`]="{ item }">
+        {{ capitalize(item.title) }}
       </template>
     </v-data-table>
   </div>
 </template>
 
 <script>
+import {mapActions, mapGetters} from "vuex";
+
 export default {
   name: "ExpenseTable",
   data() {
     return {
       search: '',
-      calories: '',
-      desserts: [
-        {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: 1,
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: 1,
-        },
-        {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: 7,
-        },
-        {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: 8,
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: 16,
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: 0,
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: 2,
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: 45,
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: 22,
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: 6,
-        },
-      ],
     }
   },
   computed: {
+    ...mapGetters(['getExpenses']),
     headers() {
       return [
-        {
-          text: 'Dessert (100g serving)',
-          align: 'start',
-          sortable: false,
-          value: 'name',
-        },
-        {
-          text: 'Calories',
-          value: 'calories',
-          filter: value => {
-            if (!this.calories) return true
-
-            return value < parseInt(this.calories)
-          },
-        },
-        {text: 'Fat (g)', value: 'fat'},
-        {text: 'Carbs (g)', value: 'carbs'},
-        {text: 'Protein (g)', value: 'protein'},
-        {text: 'Iron (%)', value: 'iron'},
+        {text: 'Nazwa', value: 'title'},
+        {text: 'Wartość', value: 'amount'},
+        {text: 'Data', value: 'data'},
+        {text: 'Kategoria', value: 'category'},
       ]
     },
   },
   methods: {
-    // filterOnlyCapsText(value, search, item) {
-    //   return value != null &&
-    //       search != null &&
-    //       typeof value === 'string' &&
-    //       value.toString().toLocaleUpperCase().indexOf(search) !== -1
-    // },
+    ...mapActions(['loadExpenses']),
+    capitalize(str) {
+      return str[0].toUpperCase() + str.slice(1);
+    }
+  },
+  created() {
+    this.loadExpenses();
   },
 }
 </script>
